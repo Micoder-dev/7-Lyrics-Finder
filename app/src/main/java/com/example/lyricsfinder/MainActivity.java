068687e4ef279c417a634365f14bd3bbaef36288
Micoder-dev <micoder.com@gmail.com>
2022-07-22 19:44:15 +0530

    get and display complete JSON data

--- app/src/main/java/com/example/lyricsfinder/MainActivity.java
+++ app/src/main/java/com/example/lyricsfinder/MainActivity.java
@@ -2,13 +2,90 @@ package com.example.lyricsfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtArtistName, edtSongName;
+    Button btnGetLyrics;
+    TextView txtLyrics;
+    ProgressDialog loadingBar;
+
+    //https://api.lyrics.ovh/v1/Rihanna/Diamonds#
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtArtistName = findViewById(R.id.edtArtistName);
+        edtSongName = findViewById(R.id.edtSongName);
+        btnGetLyrics = findViewById(R.id.btnGetLyrics);
+        txtLyrics = findViewById(R.id.txtLyrics);
+        loadingBar = new ProgressDialog(this);
+
+
+
+        btnGetLyrics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                loadingBar.setTitle("Searching...");
+                loadingBar.setMessage("Please wait, your lyrics is loading...");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+
+                String url = "https://api.lyrics.ovh/v1/" + edtArtistName.getText().toString() + "/" + edtSongName.getText().toString();
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        loadingBar.dismiss();
+
+                        Toast.makeText(MainActivity.this, "onResponse called", Toast.LENGTH_SHORT).show();
+
+                        txtLyrics.setText(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        loadingBar.dismiss();
+
+                        Toast.makeText(MainActivity.this, "onErrorResponse : " + error.toString(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                //extending loading timeout
+                jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                        100000,
+                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+                requestQueue.add(jsonObjectRequest);
+
+            }
+        });
+
     }
 }
